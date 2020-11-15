18c09dd22dee45bf6d5d34ceb09e4ddebfb80ae7
Marcin Banaś <mb056625@student.ath.edu.pl>
2020-11-15 11:13:13 +0100

    kod

---
+++ kolokwium/Kontakt.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kolokwium
+{
+    class Kontakt
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int NrTelefonu { get; set; }
+        public int DataUrodzenia { get; set; }
+
+        Kontakt(string imie,string nazwisko,int nrtelefonu,int dataurodzenia)
+        {
+            try
+            {
+                if (char.IsUpper(imie, 1) && char.IsUpper(nazwisko,1))
+                {
+                    imie = Imie;
+                    nazwisko = Nazwisko;
+                    nrtelefonu = NrTelefonu;
+                    dataurodzenia = DataUrodzenia;
+                }
+
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Bledne dane"); ;
+            }
+            imie = Imie;
+            nazwisko = Nazwisko;
+            nrtelefonu = NrTelefonu;
+            dataurodzenia = DataUrodzenia;
+        }
+    }
+}
--- kolokwium/Program.cs
+++ kolokwium/Program.cs
@@ -2,11 +2,42 @@
 
 namespace kolokwium
 {
+
     class Program
     {
+        public static void Mechanizm()
+        {
+            int suma;
+            string czas;
+            czas = Console.ReadLine();
+                
+            
+        }
+        public static void Tablica()
+        {
+            int[,] tab = new int[4, 10];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    tab[i, j] = (1);
+                }
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.WriteLine(tab[i, j]); ;
+                }
+
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Tablica();
         }
+
+
     }
 }
